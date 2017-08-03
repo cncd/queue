d56b306445bc06afebcb79215e3a1534a1351be1
SimonXming <simon_xu@outlook.com>
2017-08-03 17:59:11 +0800

    Update redis/opts.go comments.

--- redis/opts.go
+++ redis/opts.go
@@ -6,37 +6,41 @@ type Options struct {
 	password          string
 	db                int
 	penddingQueueName string
-	hostIdentity      string // For saving multiple-server running state in different redis key
+	hostIdentity      string
 }
 
 // Option configures the Redis client.
 type Option func(*Options)
 
-// WithProject configures the Pubsub client with the named project.
+// WithAddr configures Redis address.
 func WithAddr(addr string) Option {
 	return func(opts *Options) {
 		opts.addr = addr
 	}
 }
 
+// WithPassword configures Redis password.
 func WithPassword(password string) Option {
 	return func(opts *Options) {
 		opts.password = password
 	}
 }
 
+// WithDB configures Redis DB.
 func WithDB(db int) Option {
 	return func(opts *Options) {
 		opts.db = db
 	}
 }
 
+// WithPenddingQueueName configures Redis key where saveing pendding tasks.
 func WithPenddingQueueName(queueName string) Option {
 	return func(opts *Options) {
 		opts.penddingQueueName = queueName
 	}
 }
 
+// WithHostIdentity saving multiple-server running task in different redis key.
 func WithHostIdentity(identity string) Option {
 	return func(opts *Options) {
 		opts.hostIdentity = identity
